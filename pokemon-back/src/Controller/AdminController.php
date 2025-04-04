570e6df06b53efde594f206a50e8f6125f04e4bd
Cedric Chimot <cedric_om10@yahoo.fr>
2025-04-04 11:04:21 +0200

    adaptation controller admin

--- pokemon-back/src/Controller/AdminController.php
+++ pokemon-back/src/Controller/AdminController.php
@@ -15,15 +15,45 @@ class AdminController extends AbstractController
   }
 
   #[Route('/admin-boites-pokedex', name: 'admin_boite_pokedex')]
-  public function boitesPokedex(): Response
+  public function adminBoitesPokedex(): Response
   {
     return $this->render('admin/boites-pokedex.html.twig');
   }
 
   #[Route('/admin-shiny', name: 'admin_shiny')]
-  public function shinyList(): Response
+  public function adminShiny(): Response
   {
     return $this->render('admin/shiny.html.twig');
+  }
+
+  #[Route('/admin-boites-shiny', name: 'admin_boite_shiny')]
+  public function adminBoitesShiny(): Response
+  {
+    return $this->render('admin/boites-shiny.html.twig');
+  }
+
+  #[Route('/admin-dresseurs', name: 'admin_dresseurs')]
+  public function adminDresseurs(): Response
+  {
+    return $this->render('admin/dresseurs.html.twig');
+  }
+
+  #[Route('/admin-genres', name: 'admin_genres')]
+  public function adminGenres(): Response
+  {
+    return $this->render('admin/genres.html.twig');
+  }
+
+  #[Route('/admin-types', name: 'admin_types')]
+  public function adminTypes(): Response
+  {
+    return $this->render('admin/types.html.twig');
+  }
+
+  #[Route('/admin-attaques', name: 'admin_attaques')]
+  public function adminAttaques(): Response
+  {
+    return $this->render('admin/attaques.html.twig');
   }
 
 }
