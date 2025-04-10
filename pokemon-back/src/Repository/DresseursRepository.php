788bc5a80b9f1b86adbd5915f11a2d5e398a3e07
Cedric Chimot <cedric_om10@yahoo.fr>
2025-04-10 11:42:49 +0200

    adaptation repository dresseurs

--- pokemon-back/src/Repository/DresseursRepository.php
+++ pokemon-back/src/Repository/DresseursRepository.php
@@ -16,28 +16,101 @@ class DresseursRepository extends ServiceEntityRepository
     parent::__construct($registry, Dresseurs::class);
   }
 
-  //    /**
-  //     * @return Dresseurs[] Returns an array of Dresseurs objects
-  //     */
-  //    public function findByExampleField($value): array
-  //    {
-  //        return $this->createQueryBuilder('d')
-  //            ->andWhere('d.exampleField = :val')
-  //            ->setParameter('val', $value)
-  //            ->orderBy('d.id', 'ASC')
-  //            ->setMaxResults(10)
-  //            ->getQuery()
-  //            ->getResult()
-  //        ;
-  //    }
+  /**
+   * Dresseurs avec nbPokemon > 0 et nom != 'Event'
+   * @return Dresseurs[]
+   */
+  public function findAllDresseursReduit(): array
+  {
+    return $this->createQueryBuilder('d')
+      ->where('d.nbPokemon > 0')
+      ->andWhere('d.nomDresseur != :event')
+      ->setParameter('event', 'Event')
+      ->getQuery()
+      ->getResult();
+  }
 
-  //    public function findOneBySomeField($value): ?Dresseurs
-  //    {
-  //        return $this->createQueryBuilder('d')
-  //            ->andWhere('d.exampleField = :val')
-  //            ->setParameter('val', $value)
-  //            ->getQuery()
-  //            ->getOneOrNullResult()
-  //        ;
-  //    }
+  /**
+   * Dresseurs de la région 1 (non découpés)
+   * @return Dresseurs[]
+   */
+  public function findAllDresseursByRegion1(): array
+  {
+    return $this->createQueryBuilder('d')
+      ->where('d.regionDresseur IS NOT NULL')
+      ->andWhere('d.regionDresseur.idRegionDresseur = 1')
+      ->andWhere('d.nbPokemon > 0')
+      ->getQuery()
+      ->getResult();
+  }
+
+  /**
+   * Dresseurs région 1 - Partie 1 (id 1 à 40)
+   * @return Dresseurs[]
+   */
+  public function findAllDresseursByRegion1Part1(): array
+  {
+    return $this->createQueryBuilder('d')
+      ->where('d.regionDresseur.idRegionDresseur = 1')
+      ->andWhere('d.id BETWEEN 1 AND 40')
+      ->andWhere('d.nbPokemon > 0')
+      ->getQuery()
+      ->getResult();
+  }
+
+  /**
+   * Dresseurs région 1 - Partie 2 (id 41 à 81)
+   * @return Dresseurs[]
+   */
+  public function findAllDresseursByRegion1Part2(): array
+  {
+    return $this->createQueryBuilder('d')
+      ->where('d.regionDresseur.idRegionDresseur = 1')
+      ->andWhere('d.id BETWEEN 41 AND 81')
+      ->andWhere('d.nbPokemon > 0')
+      ->getQuery()
+      ->getResult();
+  }
+
+  /**
+   * Dresseurs de la région 2
+   * @return Dresseurs[]
+   */
+  public function findAllDresseursRegion2(): array
+  {
+    return $this->createQueryBuilder('d')
+      ->where('d.regionDresseur.idRegionDresseur = 2')
+      ->andWhere('d.regionDresseur IS NOT NULL')
+      ->andWhere('d.nbPokemon > 0')
+      ->getQuery()
+      ->getResult();
+  }
+
+  /**
+   * Dresseurs de la région 3
+   * @return Dresseurs[]
+   */
+  public function findAllDresseursRegion3(): array
+  {
+    return $this->createQueryBuilder('d')
+      ->where('d.regionDresseur.idRegionDresseur = 3')
+      ->andWhere('d.regionDresseur IS NOT NULL')
+      ->andWhere('d.nbPokemon > 0')
+      ->getQuery()
+      ->getResult();
+  }
+
+  /**
+   * Compter les dresseurs (hors id 119 à 140, et nbPokemon > 0)
+   * @return int
+   */
+  public function countDresseurs(): int
+  {
+    return (int) $this->createQueryBuilder('d')
+      ->select('COUNT(d.id)')
+      ->where('(d.id < 119 OR d.id > 140)')
+      ->andWhere('d.nbPokemon > 0')
+      ->getQuery()
+      ->getSingleScalarResult();
+  }
 }
